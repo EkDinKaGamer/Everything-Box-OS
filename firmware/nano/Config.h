4fb6f5e14abf89a801c60498454b0e76288fa6b2
EkDinKaGamer <aaravpadole506@gmail.com>
2026-07-25 13:20:42 +0530

    EBOS Core v0.1 - Add Nano hardware configuration

---
+++ firmware/nano/Config.h
@@ -0,0 +1,24 @@
+#pragma once
+
+// Everything Box OS - Arduino Nano Configuration
+
+#define MAX7219_DIN 11
+#define MAX7219_CLK 13
+#define MAX7219_CS 10
+
+#define TM1637_CLK 2
+#define TM1637_DIO 3
+
+#define BUZZER_PIN 4
+
+#define RGB_R_PIN 5
+#define RGB_G_PIN 6
+#define RGB_B_PIN 9
+
+#define SERVO_PIN 7
+
+#define JOYSTICK_X A0
+#define JOYSTICK_Y A1
+#define JOYSTICK_SW 8
+
+#define EBOS_VERSION "0.1.0"
