dadd0581f23aef8ed354428dacbc5288a2c8c431
EkDinKaGamer <aaravpadole506@gmail.com>
2026-07-25 13:16:37 +0530

    EBOS Core v0.1 - Add Nano and ESP8266 firmware base

---
+++ firmware/nano/EBOS_Nano.ino
@@ -0,0 +1,16 @@
+/*
+  Everything Box OS - Arduino Nano Core
+  Version: 0.1.0
+
+  Initial firmware skeleton.
+*/
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Everything Box OS Nano Core Started");
+}
+
+void loop() {
+  // Main EBOS scheduler will be added here.
+  delay(100);
+}
