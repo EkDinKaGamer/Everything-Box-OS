8ac40f307fbd48441e53f40da610310a0356fe1e
EkDinKaGamer <aaravpadole506@gmail.com>
2026-07-25 13:20:52 +0530

    EBOS Core v0.1 - Add ESP8266 communication base

---
+++ firmware/esp8266/EBOS_ESP8266.ino
@@ -0,0 +1,14 @@
+/*
+  Everything Box OS - ESP8266 Core
+  WiFi and communication layer base
+*/
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("EBOS ESP8266 Started");
+}
+
+void loop() {
+  // Future WiFi dashboard and Nano communication tasks
+  delay(100);
+}
